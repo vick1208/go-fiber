ad6c7e9dcd1d7818c2a51fc9b451f718d4c61967
Vicky AS <vickyhuang808@gmail.com>
2024-01-02 18:44:14 +0700

    initialize commit

---
+++ fiber_test.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+var app = fiber.New()
+
+func TestRouteHelloWorld(t *testing.T) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+
+	request := httptest.NewRequest("GET", "/", nil)
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello World", string(bytes))
+
+}
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Query("name", "Guest")
+		return c.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Dion", nil)
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Dion", string(bytes))
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(bytes))
+}
+
+func TestHttpRequest(t *testing.T) {
+	app.Get("/req", func(c *fiber.Ctx) error {
+		first := c.Get("firstname")
+		last := c.Cookies("lastname")
+		return c.SendString("Hello " + first + " " + last)
+	})
+	request := httptest.NewRequest("GET", "/req", nil)
+	request.Header.Set("firstname", "Eko")
+	request.AddCookie(&http.Cookie{Name: "lastname", Value: "Soegianto"})
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Eko Soegianto", string(bytes))
+}
+
+func TestRouteParameter(t *testing.T) {
+	app.Get("/users/:userId/orders/:orderId", func(c *fiber.Ctx) error {
+		userId := c.Params("userId")
+		orderId := c.Params("orderId")
+		return c.SendString("Order " + orderId + " from " + userId)
+	})
+	request := httptest.NewRequest("GET", "/users/eko/orders/2", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Order 2 from eko", string(bytes))
+}
+func TestFormReq(t *testing.T) {
+	app.Post("/hi", func(c *fiber.Ctx) error {
+		name := c.FormValue("name")
+		return c.SendString("Hi " + name)
+	})
+	body := strings.NewReader("name=Eko")
+	request := httptest.NewRequest("POST", "/hi", body)
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hi Eko", string(bytes))
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World !!!!!")
+	})
+
+	err := app.Listen("localhost:3000")
+
+	if err != nil {
+		panic(err)
+	}
+}
