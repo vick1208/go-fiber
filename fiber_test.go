3d58441d9aee17d659e2116761b35df3141620c1
Vicky AS <vickyhuang808@gmail.com>
2024-01-04 12:17:18 +0700

    http response

--- fiber_test.go
+++ fiber_test.go
@@ -217,3 +217,20 @@ func TestBodyParserForm(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Register Success Eric", string(bytes))
 }
+func TestResponseJSON(t *testing.T) {
+	app.Get("/user", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"username": "khan",
+			"name":     "Eko Khan",
+		})
+	})
+	request := httptest.NewRequest("GET", "/user", nil)
+	request.Header.Set("Accept", "application/json")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"name":"Eko Khan","username":"khan"}`, string(bytes))
+}
