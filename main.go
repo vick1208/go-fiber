1fbeabdbdcd5e3620e43e8b1d610c3ce14647280
Vicky AS <vickyhuang808@gmail.com>
2024-01-17 17:44:15 +0700

    prefork

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,11 +12,18 @@ func main() {
 		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
+		Prefork:      true,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World !!!!!")
 	})
+
+	if fiber.IsChild() {
+		fmt.Println("This is child process")
+	} else {
+		fmt.Println("This is parent process")
+	}
 
 	err := app.Listen("localhost:3000")
 
